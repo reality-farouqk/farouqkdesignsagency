2ae31e8e4fd415c0487510f4a70a256ed09c6eae
Umar sulaiman mailafiya <mailafiyaumarsulaiman@gmail.com>
2026-06-27 14:43:41 +0100

    corected exclusion andincluded other measures

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   webpack: (config) => {
     const ignored = Array.isArray(config.watchOptions?.ignored)
       ? config.watchOptions.ignored
